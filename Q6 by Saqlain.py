48ba8b582681a0fdc7b19160bea1c9dbc0dbb4c8
saqlainmustak1998 <46235965+saqlainmustak1998@users.noreply.github.com>
2018-12-29 14:44:07 +0530

    Add files via upload

---
+++ Q6 by Saqlain.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=input("Enter the names\n")
+b=input("Enter the name\n")
+c=input("Enter the name\n")
+m=input("Enter the name\n")
+n=input("Enter the name\n")
+d=[a,b,c,m,n]
+print(d)
+d.sort()
+print(d)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
