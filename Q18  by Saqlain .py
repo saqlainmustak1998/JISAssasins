5f46b6b7b50b4485748d459b82d656ea780f1916
saqlainmustak1998 <46235965+saqlainmustak1998@users.noreply.github.com>
2018-12-29 14:52:31 +0530

    Add files via upload

---
+++ Q18  by Saqlain .py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=int(input("Enter the number\n"))
+y=int(input("Enter the power\n"))
+i=y
+a=1
+while i!=0:
+    a*=x
+    i-=1
+print("Result is\n",a)
+
+
+# In[ ]:
+
+
+
+
