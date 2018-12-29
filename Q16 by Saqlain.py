5b9e255cc722c1b8bd198c066f52063e6bd85391
saqlainmustak1998 <46235965+saqlainmustak1998@users.noreply.github.com>
2018-12-29 13:58:02 +0530

    Add files via upload

---
+++ Q16 by Saqlain.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+num=input("Enter a charecter\n")
+b=ord(num)
+print("Ascii value is\n",b)
+m=chr(b+1)
+print("Next charecter is\n",m)
+x=chr(b-1)
+print("Previous charecter is\n",x)
+
+
+# In[ ]:
+
+
+
+
