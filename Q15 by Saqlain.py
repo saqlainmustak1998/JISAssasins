bdab353e0cf7564b8e7fef5a0b3e9006c2db352d
saqlainmustak1998 <46235965+saqlainmustak1998@users.noreply.github.com>
2018-12-29 12:59:09 +0530

    Add files via upload

---
+++ Q15 by Saqlain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+x=int(input("Enter the value of x\n"))
+y=int(input("Enter the value of y\n"))
+z=int(input("Enter the valueof z\n"))
+s=(x**(y+z))
+print(s)
+
+
+# In[ ]:
+
+
+
+
