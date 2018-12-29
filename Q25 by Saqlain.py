fa8142c558fb83e6a69a247d06d5a4cf487b3b97
saqlainmustak1998 <46235965+saqlainmustak1998@users.noreply.github.com>
2018-12-29 15:38:55 +0530

    Add files via upload

---
+++ Q25 by Saqlain.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+ch=input("Enter the Charecter\n")
+if ((ch>='a' and ch<='z') or (ch>='A' and ch<='B' )):
+    print(ch,"is an Alphabet")
+else:
+    print(ch,"is a digit")
+    
+
+
+# In[ ]:
+
+
+
+
